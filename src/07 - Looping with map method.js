44364888fb6bb39f6e2146474ad87e44b6bc5668
3imed-jaberi <imed_jebari@hotmail.fr>
2021-02-12 22:47:35 +0100

    add src dir --files --xx 📦 ..

---
+++ src/07 - Looping with map method.js
@@ -0,0 +1,12 @@
+// looping with map method
+
+// // default data
+// const fruits = [
+//   'Apple 🍎',
+//   'Pear 🍐',
+//   'Banana 🍌',
+//   'Grape 🍇',
+//   'Strawberries 🍓',
+//   'Potato 🥔',
+//   'Avocado 🥑',
+// ]
